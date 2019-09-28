28763ceee8e74f24b09a8f1e4a01c8b45648f84d
SlawcioXXX <nordowikaszeba@gmail.com>
2019-09-28 19:06:18 +0200

    Added menu to application

--- Calculator3000/Program.cs
+++ Calculator3000/Program.cs
@@ -10,6 +10,25 @@ namespace Calculator3000
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.WriteLine("0. Exit");
+                Console.WriteLine("1. Add");
+                Console.WriteLine("2. Diff");
+                int menuOption = Convert.ToInt32(Console.ReadLine());
+                if (menuOption == 0)
+                {
+                    break;
+                }
+                else if (menuOption == 1)
+                {
+
+                }
+                else if (menuOption == 2)
+                {
+
+                }
+            }
         }
     }
 }
