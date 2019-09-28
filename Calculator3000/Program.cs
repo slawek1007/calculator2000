719a048d34a2e0431d138e6d20e97ad6c99d3bb6
SlawcioXXX <nordowikaszeba@gmail.com>
2019-09-28 19:32:58 +0200

    Refactored application and reorganized menu

--- Calculator3000/Program.cs
+++ Calculator3000/Program.cs
@@ -12,19 +12,20 @@ namespace Calculator3000
         {
             while (true)
             {
-                Console.WriteLine("0. Exit");
-                Console.WriteLine("1. Add");
-                Console.WriteLine("2. Diff");
                 int menuOption = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("0. Exit");
                 if (menuOption == 0)
                 {
                     break;
                 }
-                else if (menuOption == 1)
+                Console.WriteLine("1. ADD");
+                if (menuOption == 1)
                 {
 
                 }
-                else if (menuOption == 2)
+                Console.WriteLine("2. DIFF");
+                if (menuOption == 2)
                 {
 
                 }
